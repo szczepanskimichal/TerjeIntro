785d71a818175146ff41917a490c7f555b3f8e9c
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-05-21 08:14:25 +0200

    Jeg begynner

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ShowPlaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************");
+            Console.WriteLine("Navn: Stavern");
+            Console.WriteLine("Kommune: Larvik");
+            Console.WriteLine("Fylke: Vestfold");
+            Console.WriteLine("************************");
+        }
+    }
+}
