757c74cffb6061d6fbbaaa6089f5111810caa0e5
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-05-23 11:13:29 +0200

    Separators, PadLeft

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
-﻿namespace ShowPlaces
+﻿using System.Reflection.Emit;
+
+namespace ShowPlaces
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("************************");
-            Console.WriteLine("Navn: Stavern");
-            Console.WriteLine("Kommune: Larvik");
-            Console.WriteLine("Fylke: Vestfold");
+            ShowSeparator();
+            var odstep = 20;
+            //var label = "Navn";
+            //var fieldValue = "Stavern";
+            ShowFieldNameAndValue("Navn", odstep, "Stavern");
+            ShowFieldNameAndValue("Kommune", odstep, "Larvik");
+            ShowFieldNameAndValue("Fylke", odstep, "Vestfold");
+
+            ShowSeparator();
+        }
+
+        static void ShowFieldNameAndValue(string label, int odstep, string fieldValue)
+        {
+            odstep -= label.Length;
+            Console.WriteLine(" " + label + " : " + "".PadLeft(odstep, ' ') + fieldValue);
+        }
+
+        static void ShowSeparator()
+        {
             Console.WriteLine("************************");
         }
     }
